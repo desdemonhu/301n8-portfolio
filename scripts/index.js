8ac7f7f9af59ea79ff53429ab507ea87ea050ddc
Raegan Millhollin <desdemonhu@gmail.com>
2017-07-26 17:55:13 -0700

    Project constructor

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+///array of all projects
+var projects = [];
+///constructor function for project information
+function Project(name, url, image, description){
+  this.name = name;
+  this.url = url;
+  this.image = image;
+  this.description = description;
+
+  ///automatically push new one to the array
+  projects.push(this);
+};
