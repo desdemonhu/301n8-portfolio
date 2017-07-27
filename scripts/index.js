96998cadde4d55aebccfcde3debbf6b7beacd30f
Raegan Millhollin <desdemonhu@gmail.com>
2017-07-27 09:19:49 -0700

    hamburger menu toggles!

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 ///array of all projects
 var projects = [];
 ///constructor function for project information
@@ -11,4 +10,19 @@ function Project(name, url, image, description){
 
   ///automatically push new one to the array
   projects.push(this);
-};
+}
+
+
+///hamburger menu closes if you click it a second time
+$(document).ready(function(){
+  var click = 0;
+  $('.icon-menu').click(function(){
+    click += 1;
+    if(click % 2 === 0 ){
+      $('.navigation ul').css('display', 'none');
+      console.log(click);
+    }else {
+      $('.navigation ul').css('display', 'inline');
+    }
+  });
+});
