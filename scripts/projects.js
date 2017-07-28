e83e94b5b92523e41ab6c110115f3e1838ee62b8
Raegan Millhollin <desdemonhu@gmail.com>
2017-07-27 20:41:09 -0700

    created projects.js file and put placeholders there

---
+++ scripts/projects.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var project1 = new Project('Portfolio', '#', '', 'A portfolio of projects, focused on mobile-first design, SMACSS design principles, and reusability accross multiple projects.');
+var project2 = new Project('Project Two', '#', '', 'A placeholder project');
+var project3 = new Project('Project Three', '#', '', 'A placeholder project in third place');
