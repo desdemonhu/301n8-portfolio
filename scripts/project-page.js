bf12a94a5406a947ca6c447e48bef41e02ecc7db
Raegan Millhollin <desdemonhu@gmail.com>
2017-07-30 17:54:48 -0700

    refectored some code into functions instead of in document.ready, added condition if published date is in the future it displays in progress instead of days ago

--- scripts/project-page.js
+++ scripts/project-page.js
@@ -1,5 +1,6 @@
 'use strict';
 
+///Sorts the projects array by most recent Published date
 function sortProjects(){
   projects.sort(function(a,b){
     var aDate = new Date(a.published);
@@ -8,21 +9,30 @@ function sortProjects(){
   })
 }
 
+///display projects on project page
+Project.prototype.displayProjectsPage = function(){
+  var $project = $('#projects').clone();
+  //published number of days ago calculations
+
+
+  $project.removeClass('template');
+  $project.find('#project-image').attr('src', this.image);
+  $project.find('#project-name').text(this.name);
+  $project.find('#project-url').attr('href', this.url).text(this.url);
+  $project.find('#project-description').text(this.description);
+  $project.attr('data-language', this.language);
+  if(this.calculateDaysAgo() === 0){
+    ///if Published date is in the future just display in progress
+    $project.find('#published').parent().text('In Progress');
+  }else {
+    $project.find('#published').text(this.calculateDaysAgo());
+  }
+  $('body').append($project);
+}
+
 $(document).ready(function(){
   sortProjects();
-
   projects.forEach(function(item){
-    var $project = $('#projects').clone();
-    //published number of days ago calculations
-
-
-    $project.removeClass('template');
-    $project.find('#project-image').attr('src', item.image);
-    $project.find('#project-name').text(item.name);
-    $project.find('#project-url').attr('href', item.url).text(item.url);
-    $project.find('#project-description').text(item.description);
-    $project.attr('data-language', item.language);
-    $project.find('#published').text(item.calculateDaysAgo());
-    $('body').append($project);
+    item.displayProjectsPage();
   });
 });
