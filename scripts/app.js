cb3e6e7370079887a3cc075cb0dbb41f74941333
Raegan Millhollin <desdemonhu@gmail.com>
2017-07-30 19:34:05 -0700

    made project page part of index.html and all that entails

--- scripts/app.js
+++ scripts/app.js
@@ -37,6 +37,25 @@ Project.prototype.calculateDaysAgo = function(){
   }
 }
 
+///display projects on project page
+Project.prototype.displayProjectsPage = function(){
+  var $project = $('#projects').clone();
+  //published number of days ago calculations
+  $project.addClass('projects-displayed');
+  $project.find('#project-image').attr('src', this.image);
+  $project.find('#project-name').text(this.name);
+  $project.find('#project-url').attr('href', this.url).text(this.url);
+  $project.find('#project-description').text(this.description);
+  $project.attr('data-language', this.language);
+  if(this.calculateDaysAgo() === 0){
+    ///if Published date is in the future just display in progress
+    $project.find('#published').parent().text('In Progress');
+  }else {
+    $project.find('#published').text(this.calculateDaysAgo());
+  }
+  $('body').append($project);
+}
+
 ///click event for project Next button
 function featureDisplay(){
   var index = projects.length - 1;
@@ -75,7 +94,22 @@ function hamburgerMenu(){
   });
 }
 
+///Sorts the projects array by most recent Published date
+function sortProjects(){
+  projects.sort(function(a,b){
+    var aDate = new Date(a.published);
+    var bDate = new Date(b.published);
+    return bDate - aDate;
+  })
+}
+
 $(document).ready(function(){
+  sortProjects();
   hamburgerMenu();
   featureDisplay();
+  portfolioClick();
+
+  for(var i = 0; i < projects.length; i++){
+    projects[i].displayProjectsPage();
+  }
 });
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,20 @@
 'use strict';
 
-var navigationString = '<ul><li><a href="https://github.com/desdemonhu">GitHub<a></li><li><a href="projects.html">Project Portfolio</a></li></ul><ul><li><a href="https://www.linkedin.com/in/raegan-millhollin-2a3647b">Linkedin</a></li><li><a href="http://desdemonhu.deviantart.com/">DeviantArt</a></li></ul>'
+var navigationString = '<ul><li><a href="https://github.com/desdemonhu">GitHub<a></li><li><a href="#">Project Portfolio</a></li></ul><ul><li><a href="https://www.linkedin.com/in/raegan-millhollin-2a3647b">Linkedin</a></li><li><a href="http://desdemonhu.deviantart.com/">DeviantArt</a></li></ul>'
+
+function portfolioClick(){
+  $('li:contains("Project")').on('click', function(event){
+    event.preventDefault();
+    $('#project-feature').hide();
+    $('#intro').hide();
+    $('#footer').prev().hide();
+    $('#footer').hide();
+    ///Shows project list
+    $('.projects-displayed').each(function(){
+      $(this).removeClass('template');
+    });
+  })
+}
 
 $(document).ready(function(){
   $('.navigation').html(navigationString);
