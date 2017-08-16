423bbcfaf6b294a24e930468fdcecbc25d0e82ef
Raegan Millhollin <desdemonhu@gmail.com>
2017-08-16 16:45:17 -0700

    express server stuff works for me now

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser').urlencoded({extended: true});
+// const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 4000;
 
 
