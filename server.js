86e14e04d8f3e083a5b1a22a2a47cda9daffa93b
Raegan Millhollin <desdemonhu@gmail.com>
2017-08-16 16:32:41 -0700

    Put index.html in the right folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 app.use(express.static('public'));
