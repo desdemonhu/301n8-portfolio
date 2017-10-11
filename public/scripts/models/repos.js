9e5b4ae49aaaa4a1e300ca31ac0fe445ad80500b
Raegan Millhollin <desdemonhu@gmail.com>
2017-10-11 12:29:30 -0700

    Hopefully fixed repos

--- public/scripts/models/repos.js
+++ public/scripts/models/repos.js
@@ -7,23 +7,25 @@ var portfolio = portfolio || {};
 
   repos.fetchData = function(){
     // TODO: This added the whole array each time you go to the page. But if you refresh it's fine. Fix this!
-    $.get('/github/users/desdemonhu/repos')
-    .then(results =>{
-      var modifiedResults = results.sort(function(a,b){
-        let aDate = new Date(a['updated_at']);
-        let bDate = new Date(b['updated_at']);
-        return bDate - aDate;
-      }).reduce(function(acc, value, index){
-        if(index < 5){
-          acc.push(value);
-        }
-        return acc;
-      },[]);
-      modifiedResults.forEach(repo =>{
-        $('#repos').append(repos.repoTemplate(repo));
-      })
-    }, err =>{console.log(err);
-    });
+    if(!($('#repos').children().length)){
+      $.get('/github/users/desdemonhu/repos')
+      .then(results =>{
+        var modifiedResults = results.sort(function(a,b){
+          let aDate = new Date(a['updated_at']);
+          let bDate = new Date(b['updated_at']);
+          return bDate - aDate;
+        }).reduce(function(acc, value, index){
+          if(index < 5){
+            acc.push(value);
+          }
+          return acc;
+        },[]);
+        modifiedResults.forEach(repo =>{
+          $('#repos').append(repos.repoTemplate(repo));
+        })
+      }, err =>{console.log(err);
+      });
+    }
   }
   app.repos = repos;
 })(portfolio);
